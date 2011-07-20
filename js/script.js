7c7ca82856e907cfb38259aa4c961998defc7281
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-07-20 16:08:15 +0200

    update h tag styled for mobile devices using media queries

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function(){
+	jQuery(".code").hover(function() {
+	    jQuery(this)
+	    .stop(true, false)
+	    .css({
+	        zIndex: "100",
+	        
+	    })
+    });
+	
 	//jQuery(".code").hover(function() {
 	//var width = jQuery("code", this).width();
 	//var pad = width + 30;
